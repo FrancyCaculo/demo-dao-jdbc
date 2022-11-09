378f0e14ec81e01ea4f0a1e3b8a6632b5f5a31df
Francycaculo <francycaculo@hotmail.com>
2022-11-08 23:52:27 -0300

    Department entity class

---
+++ src/application/program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.department;
+
+public class program {
+
+	public static void main(String[] args) {
+		department obj = new department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
